f23b55dbb7dea008a02db5cc0c01d14d0117a30d
rahul-konar27 <rahul-konar27@users.noreply.github.com>
2020-07-11 18:18:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/locationfinder/MapsActivity.java
@@ -0,0 +1,37 @@
+package com.example.locationfinder;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        // Add a marker in the location and move the camera
+        LatLng PaavaiPolytechnic = new LatLng(11.3990, 78.1607);
+        mMap.addMarker(new MarkerOptions().position(PaavaiPolytechnic).title("Paavai Polytechnic College"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(PaavaiPolytechnic));
+    }
+}
